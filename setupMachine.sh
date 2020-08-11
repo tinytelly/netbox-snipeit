78fe28c87d77154a554e0716e4a253d61e60480c
mw <not@set.com>
2020-08-11 15:40:08 +1000

    -

--- setupMachine.sh
+++ setupMachine.sh
@@ -19,6 +19,6 @@ systemctl start docker
 docker --version
 
 # Install docker-compose
-curl -L https://github.com/docker/compose/releases/download/1.25.3/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 docker-compose -version
