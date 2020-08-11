60b7e834dac1c4e57f273ee90a691c0f3f0beb0b
mw <not@set.com>
2020-08-11 16:45:24 +1000

    -

--- README.md
+++ README.md
@@ -14,5 +14,5 @@ to install docker and git on a Centos 8 machine.
 if you are using gcp then open port 9000 to the firewall as shown above
 ```
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker run -d -p 8020:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
 ```
--- setupMachine.sh
+++ setupMachine.sh
@@ -9,10 +9,14 @@ git --version
 sudo dnf config-manager --add-repo=https://download.docker.com/linux/centos/docker-ce.repo
 sudo dnf list docker-ce --showduplicates | sort -r
 sudo dnf install docker-ce --nobest
-
+sudo systemctl start docker
 docker --version
 
 # Install docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.26.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose -version
+
+#export DOCKER_HOST=127.0.0.1:2375
+sudo usermod -aG docker $USER
+# log out and back in at this point
