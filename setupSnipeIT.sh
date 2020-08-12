f8a1068a035f6ac6a83e78d89b866906bfdc5fa3
mw <not@set.com>
2020-08-12 15:56:23 +1000

    -

--- setupSnipeIT.sh
+++ setupSnipeIT.sh
@@ -12,4 +12,11 @@ docker-compose up
 docker ps -a # Get the container name
 docker exec -it snipe-it-container-name sh
 
-php artisan key:generate
+  php artisan key:generate ## add the key to APP_KEY=<<Fill in Later!>> in the .env file
+  exit
+  vi .env # add key
+
+#Restart with key
+docker-compose down
+docker-compose up
+
