913c42e67d9c04d031ee9e06af142514e1e0af4d
mw <not@set.com>
2020-08-12 10:05:58 +1000

    -

--- setupSnipeIT.sh
+++ setupSnipeIT.sh
@@ -2,6 +2,9 @@
 
 ## following this - https://snipe-it.readme.io/docs/docker
 
+git clone https://github.com/tinytelly/netbox-snipeit.git
+cd netbox-snipeit/
+
 docker pull snipe/snipe-it
 
-docker run --name snipe-mysql --env-file=env.list --mount source=snipesql-vol,target=/var/lib/mysql -d -P mysql:5.6
+docker run --name snipe-mysql --env-file=.env --mount source=snipesql-vol,target=/var/lib/mysql -d -P mysql:5.6
